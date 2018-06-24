537c213d8af0040b85a727dd5fd50fd0ed5f89ae
Viacheslav <viacheslav.modestov@gmail.com>
2018-06-24 14:08:13 +0300

    Change App.js and App.css, add info

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          В этом компоненте в календаре выбирается дата и по ней отображается
+      список периодов. Периоды это недельные промежутки с понедельника по
+      воскресенье, входящие в годовой отрезок времени, начинающийся с
+      выбранного числа.</p>
       </div>
     );
   }
